48764236726287c5a03a1597eb36a0382773cae5
xxx <93487277+Wwilber@users.noreply.github.com>
2022-12-07 16:51:33 -0300

    primeira Etapa reactJS

---
+++ ignite/aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,48 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+
+export function Post(){
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src="https://github.com/Wwilber.png" />
+           
+        <div className={styles.authorInfo}>
+            <strong> Wilber Ribeiro</strong>
+            <span> Web Developer</span>
+          </div>
+        </div>
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala Galera</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW</p> 
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{'  '}
+          <a href="">#nlw</a>{'  '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button> 
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+
+    </article>
+  );
+}
